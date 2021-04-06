492e19cac8986c45144e3a86f478c1c7675ebb80
Kevin Santos Guedes <kevinguedes@edu.unifor.br>
2021-04-06 13:38:23 -0300

    Continuação aula html

--- exercicios-web/html/exercicios/form/server.js
+++ exercicios-web/html/exercicios/form/server.js
@@ -7,9 +7,21 @@ const port = 3333
 app.use(bodyParser.urlencoded({ extended: true }))
 //Sem este bodyParser, retornaria undefined
 
+app.get('/usuarios', (req, res) => {
+    console.log(req.query)
+    //não recomendado para dados sensíveis -> Utilizar POST e HTTPS para dados
+    res.send('<h1>Usuário incluído via GET.</h1>')
+})
+
 app.post('/usuarios', (req, res) => {
     console.log(req.body)
-    res.send(req.body)
+    res.send('<h1>Usuário incluído.</h1>')
+})
+
+app.post('/usuarios/:id', (req, res) => {
+    console.log(req.body)
+    console.log(req.params.id)
+    res.send('<h1>Usuário alterado.</h1>')
 })
 
 app.listen(port, () => {
